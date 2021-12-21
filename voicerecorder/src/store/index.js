9a6eeb3c17a5a42e5d39c06dbfaad35a37d27938
Sepehr Norouzi <sepehr.norouzi1999@gmail.com>
2021-12-21 18:35:02 +0330

    Version 0.0.3 Store has been initialized.

--- voicerecorder/src/store/index.js
+++ voicerecorder/src/store/index.js
@@ -1,13 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+	state: {
+		userId: null,
+		userLevel: 0,
+	},
+	mutations: {
+
+    initialzieStore(state) {
+      const userId = localStorage.getItem('semnanUserId')
+      if(userId) {
+        state.userId = userId;
+        state.level =  localStorage.getItem('semnanUserLevel')
+      }
+      else {
+        state.userId =  null;
+        state.userLevel = 0;
+      }
+    },
+
+		setUserId(state, id) {
+			state.userId = id;
+		},
+		setUserLevel(state, level) {
+			state.userLevel = level;
+		},
+	},
+	actions: {
+
+    setUserId({ commit }, id) {
+      commit('setUserId', id);
+    },
+
+    setUserLevel({ commit }, level) {
+      commit('setUserLevel', level);
+    },
+
+    initialzieStore({ commit }) {
+      commit('initialzieStore');
+    }
+
   },
-  mutations: {
-  },
-  actions: {
-  },
-})
+});
